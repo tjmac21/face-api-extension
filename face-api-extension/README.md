fe13a25527ada7bf396df31d702943c73d47f0a3
tjmac21 <tjmacu@gmail.com>
2021-12-30 16:56:15 -0800

    modify readme and about page

--- face-api-extension/README.md
+++ face-api-extension/README.md
@@ -1,3 +1,5 @@
 Proof of Concept Extension that attempts to expose the brilliant face-api.js API to a chrome extension 
 
 Inspiration: https://github.com/justadudewhohacks/face-api.js
+
+React Extension boiler plate: https://github.com/nemrosim/chrome-react-extension-example
--- face-api-extension/src/routes/About.tsx
+++ face-api-extension/src/routes/About.tsx
@@ -7,6 +7,11 @@ export const About = () => {
         <div className="App">
             <header className="App-header">
                 <p>About</p>
+                <p>This is a proof-of-concept that TF.js and ML can be run from a Chrome Extension.</p>
+                <p>If you like what you see consider supporting me by donating to my </p>
+                <p><a href="https://www.paypal.com/paypalme/tjmacu">PayPal</a></p>
+                <p> or starring this project in </p>
+                <p><a href="https://github.com/tjmac21/face-api-extension/tree/master/face-api-extension">Github</a></p>
                 <button onClick={() => {
                     push('/')
                 }}>Home page
